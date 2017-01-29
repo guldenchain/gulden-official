a40d8c452e0e0d0e668dc4be54139677e806bc3d
strataghyst <g.h.degraaf@gmail.com>
2017-01-30 00:24:55 +0100

    Modified for use with bitcore

--- src/script/script.h
+++ src/script/script.h
@@ -565,6 +565,8 @@ public:
      * pay-to-script-hash transactions:
      */
     unsigned int GetSigOpCount(const CScript& scriptSig) const;
+    
+    bool IsPayToPublicKeyHash() const;
 
     bool IsPayToScriptHash() const;
     bool IsPayToWitnessScriptHash() const;
--- src/serialize.h
+++ src/serialize.h
@@ -102,6 +102,11 @@ inline void ser_writedata32(Stream& s, uint32_t obj)
     obj = htole32(obj);
     s.write((char*)&obj, 4);
 }
+template<typename Stream> inline void ser_writedata32be(Stream &s, uint32_t obj)
+{
+    obj = htobe32(obj);
+    s.write((char*)&obj, 4);
+}
 template <typename Stream>
 inline void ser_writedata64(Stream& s, uint64_t obj)
 {
@@ -128,6 +133,12 @@ inline uint32_t ser_readdata32(Stream& s)
     uint32_t obj;
     s.read((char*)&obj, 4);
     return le32toh(obj);
+}
+template<typename Stream> inline uint32_t ser_readdata32be(Stream &s)
+{
+    uint32_t obj;
+    s.read((char*)&obj, 4);
+    return be32toh(obj);
 }
 template <typename Stream>
 inline uint64_t ser_readdata64(Stream& s)
